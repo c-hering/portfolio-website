3586e61f2fe8d0b8a8613336823d7d248e4cf5f2
An0rak <phos4es@gmail.com>
2017-01-23 19:53:52 -0500

    stuff

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,4 +1,12 @@
-var activeSubstitute = function(pageName){
-  console.log('aksdl;fmakl');
-  // element.getElementById(pageName).style.font-weight = bolder;
+function loadJSON(df){
+  var dataFile = df;
+  return jQuery.getJSON(df);
+}
+
+function imageSlider(json){
+  var images;
+  for(var i = 0; i < json.length; i++){
+    images.push(json[i].picture);
+  }
+  console.log(images);
 }
